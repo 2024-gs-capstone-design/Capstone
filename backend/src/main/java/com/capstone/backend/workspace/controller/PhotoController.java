00fa965d2018bb8af9bac9e7e8c158d5d57d3c21
ooosj <tmdwn8775@gmail.com>
2024-11-23 14:30:45 +0900

    사진 가져오기 방식 getmaping으로 변경

--- backend/src/main/java/com/capstone/backend/workspace/controller/PhotoController.java
+++ backend/src/main/java/com/capstone/backend/workspace/controller/PhotoController.java
@@ -3,12 +3,22 @@ package com.capstone.backend.workspace.controller;
 import com.capstone.backend.workspace.dto.PhotoDTO;
 import com.capstone.backend.workspace.service.PhotoService;
 import com.capstone.backend.workspace.service.WorkspaceService;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -37,6 +47,9 @@ public class PhotoController {
             @RequestParam("workspaceId") Long workspaceId) {
         System.out.println("File: " + file.getOriginalFilename());
         System.out.println("Workspace ID: " + workspaceId);
+        // 파일의 MIME 타입 확인
+        String mimeType = file.getContentType();
+        System.out.println("업로드된 파일의 MIME 타입: " + mimeType);
         try {
             String filePath = photoService.uploadPhoto(file, workspaceId);
             return ResponseEntity.ok("파일 업로드 성공: " + filePath);
@@ -46,5 +59,26 @@ public class PhotoController {
         }
     }
 
+    @GetMapping("/upload/photos/{fileName:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String fileName) {
+        try {
+            Path filePath = Paths.get("/Users/oooh/git/capstone/backend/src/main/resources/static/upload/photos").resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (resource.exists() && resource.isReadable()) {
+                String contentType = Files.probeContentType(filePath);
+
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(contentType))
+                        .body(resource);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
